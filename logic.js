e2acd2f4f95bd543daaa57c4ff3130ba3eb5315a
ElisaEscola <elisabeth.martins@escola.pr.gov.br>
2026-04-28 12:35:34 -0300

    Atualizado

--- logic.js
+++ logic.js
@@ -5,32 +5,32 @@ const porta = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post('/finalizar-venda'), (req, res) => {
+app.post('/finalizar-venda', (req, res) => {
     const dadosVenda = req.body;
+    const total = dadosVenda.preco * dadosVenda.quantidade;
 
+    if (!dadosVenda.quantidade || isNaN(dadosVenda.quantidade)) {
 
-    const preco = Number(dadosVenda.preco);
-    const quantidade = Number(dadosVenda.quantidade);
-
-
-    if (!dadosVenda.quantidade || isNaN(dadosVenda.quantidade))
         return res.status(400).json({
             sucesso: false,
             erro: "Dados invalidos na quantidade!"
         })
-    const cupom = total * 0.20;
-    const total = preco * quantidade
+
+    }
+
+
     res.send(`Venda de ${dadosVenda.nome} finalizada! O total foi ${total}`)
 
-    if (dadosVenda.cupom == "PROMO20")
-
+    if (dadosVenda.cupom === "PROMO20") {
         return res.status(201).json({
             mensagem: "Venda realizada com sucesso!",
             produto: dadosVenda.produto,
             valorTotal: total,
             status: "compra confirmada confirmado"
         })
-}
+    }
+
+});
 
 app.listen(porta, () => {
     console.log(`Servidor rodando! Aguardando o formulário HTML na porta ${porta}...`);
