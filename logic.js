a79593004c8134482344119c1ce7fe7668c846b2
ElisaEscola <elisabeth.martins@escola.pr.gov.br>
2026-04-28 12:04:59 -0300

    Alteração1

--- logic.js
+++ logic.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const porta = 8080;
 
-
-
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -21,10 +17,6 @@ app.post('/finalizar-venda'), (req, res) => {
         return res.status(400).json({
             sucesso: false,
             erro: "Dados invalidos na quantidade!"
-
-
-
-
         })
     const cupom = total * 0.20;
     const total = preco * quantidade
@@ -32,10 +24,14 @@ app.post('/finalizar-venda'), (req, res) => {
 
     if (dadosVenda.cupom == "PROMO20")
 
-    return res.status(201).json({
-        mensagem: "Venda realizada com sucesso!",
-        produto: dadosVenda.produto,
-        valorTotal: total,
-        status: "compra confirmada confirmado"
-    })
+        return res.status(201).json({
+            mensagem: "Venda realizada com sucesso!",
+            produto: dadosVenda.produto,
+            valorTotal: total,
+            status: "compra confirmada confirmado"
+        })
 }
+
+app.listen(porta, () => {
+    console.log(`Servidor rodando! Aguardando o formulário HTML na porta ${porta}...`);
+});
